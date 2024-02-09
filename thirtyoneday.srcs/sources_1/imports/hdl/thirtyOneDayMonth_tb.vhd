3de842520b8a15610e25afef4c3128fd1ea938fd
TW33D0 <141987873+TW33D0@users.noreply.github.com>
2024-02-09 13:26:05 -0700

    Final Adjustments Based on Copied Project

--- thirtyoneday.srcs/sources_1/imports/hdl/thirtyOneDayMonth_tb.vhd
+++ thirtyoneday.srcs/sources_1/imports/hdl/thirtyOneDayMonth_tb.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thirtyOneDayMonth_tb.vhd (TEST BENCH)
---| AUTHOR(S)     : Capt Dan Johnson, ***Your Name Here***
---| CREATED       : 12/12/2019 Last Modified 06/24/2020
+--| AUTHOR(S)     : Capt Dan Johnson, Ty Hubert
+--| CREATED       : 12/12/2019 Last Modified 02/07/2024
 --| DESCRIPTION   : This file tests to ensure thirtyOneDayMonthMux works properly
 --|
 --|
@@ -58,11 +58,11 @@ architecture test_bench of thirtyOneDayMonth_tb is
   -- declare the component of your top-level design unit under test (UUT) (looks very similar to entity declaration)
   component thirtyoneDayMonth is
     port(
-	i_A : in std_logic;
-	i_B : in std_logic;
-	i_C : in std_logic;
-	i_D : in std_logic;
-	o_Y : out std_logic
+	   i_A : in std_logic;
+	   i_B : in std_logic;
+	   i_C : in std_logic;
+	   i_D : in std_logic;
+	   o_Y : out std_logic
     );	
   end component;
 
@@ -80,7 +80,7 @@ begin
 			i_C => w_sw(2),
 			i_B => w_sw(1),
 			i_A => w_sw(0),
-	    		o_Y => w_Y
+	    	o_Y => w_Y
         );
 	-----------------------------------------------------
 
@@ -93,8 +93,29 @@ begin
 		w_sw <= x"0"; wait for 10 ns;
             assert w_Y = '0' report "error on x0" severity failure;
         w_sw <= x"1"; wait for 10 ns;
-            assert w_Y = '1' report "error on Jan" severity failure;   
-
+            assert w_Y = '1' report "error on Jan" severity failure; 
+        w_sw <= x"2"; wait for 10 ns;
+            assert w_Y = '0' report "error on Feb" severity failure;      
+        w_sw <= x"3"; wait for 10 ns;
+            assert w_Y = '1' report "error on Mar" severity failure;  
+        w_sw <= x"4"; wait for 10 ns;
+            assert w_Y = '0' report "error on Apr" severity failure;
+        w_sw <= x"5"; wait for 10 ns;
+            assert w_Y = '1' report "error on May" severity failure;
+        w_sw <= x"6"; wait for 10 ns;
+            assert w_Y = '0' report "error on Jun" severity failure;
+        w_sw <= x"7"; wait for 10 ns;
+            assert w_Y = '1' report "error on Jul" severity failure;
+		w_sw <= x"8"; wait for 10 ns;
+            assert w_Y = '1' report "error on Aug" severity failure;
+        w_sw <= x"9"; wait for 10 ns;
+            assert w_Y = '0' report "error on Sep" severity failure;
+		w_sw <= x"A"; wait for 10 ns;
+            assert w_Y = '1' report "error on Oct" severity failure;
+		w_sw <= x"B"; wait for 10 ns;
+            assert w_Y = '0' report "error on Nov" severity failure;
+		w_sw <= x"C"; wait for 10 ns;
+            assert w_Y = '1' report "error on Dec" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
